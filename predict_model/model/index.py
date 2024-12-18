be5ddcb8f46eecbb0a9c4b81c7d9cd7e4ab60dcc
bluewarn <n5lp97@gmail.com>
2024-12-18 03:18:19 +0000

    plots 저장 경로 수정, 불필요한 코드 제거

--- predict_model/model/index.py
+++ predict_model/model/index.py
@@ -37,67 +37,56 @@ train = train.dropna(how='any')
 # 데이터 확인
 print(train.head())
 
-top_10_revenue = train.sort_values(by='revenue', ascending=False).head(10)
-print(top_10_revenue)
-
-# top_10_budget = train.sort_values(by='budget', ascending=False).head(10)
-# print(top_10_budget)
-
-# # budget 히스토그램과 KDE, 0부터 10퍼센트 구간만 자세히 보기
-# sns.histplot(train.budget, kde=True, stat="density")
-# plt.savefig("budget_히스토그램.png")
-# plt.clf()
-
-# revenue 히스토그램과 KDE, 0부터 10퍼센트 구간만 자세히 보기
-sns.histplot(train.revenue, kde=True, stat="density")
-plt.savefig("revenue_히스토그램.png")
-plt.clf()
-
 # 수치형 데이터만 선택, 상관 계수 계산.
 numeric_features = train.select_dtypes(include=[np.number])
 # 결측치 처리 (예: 평균값으로 대체)
-# numeric_features = numeric_features.fillna(method='ffill')
 correlation_matrix = numeric_features.corr()
 
 print("상관계수: ", correlation_matrix)
 
 sns.heatmap(numeric_features.corr(), cmap='YlGnBu', annot=True, linewidths = 0.2);
-plt.savefig("correlation_matrix.png")
+plt.savefig("./analysis_plots/correlation_matrix.png")
+plt.clf()
+
+print('Descriptive Stats for the runtime are:\n ', train.runtime.describe())
+sns.histplot(train.runtime, kde=True)
+plt.savefig("./analysis_plots/runtime_히스토그램.png")
 plt.clf()
 
 print('Descriptive Stats for the revenue are:\n ', train.revenue.describe())
 sns.histplot(train.revenue, kde=True)
-plt.savefig("histplot_train.revenue.png")
+plt.savefig("./analysis_plots/revenue_히스토그램.png")
 plt.clf()
 
 print('Descriptive Stats for the omdb_rate are:\n ', train.omdb_rate.describe())
 sns.histplot(train.omdb_rate, kde=True)
-plt.savefig("histplot_train.omdb_rate.png")
+plt.savefig("./analysis_plots/omdb_rate_히스토그램.png")
 plt.clf()
 
-# sns.jointplot(x = train.budget, y = train.omdb_rate);
-# plt.savefig("jointPlot_budget_omdb_rate.png")
-# plt.clf()
+sns.jointplot(x = train.revenue, y = train.omdb_rate);
+plt.savefig("./analysis_plots/revenue_X_omdb_rate_JointPlot.png")
+plt.clf()
+
 sns.jointplot(x = train.runtime, y = train.omdb_rate);
-plt.savefig("jointPlot_runtime_omdb_rate.png")
+plt.savefig("./analysis_plots/runtime_X_omdb_rate_JointPlot.png")
 plt.clf()
 
-# sns.boxplot(x=train['budget'])
-# plt.savefig("budget_박스플롯.png")
-# plt.clf()
+sns.boxplot(x=train['runtime'])
+plt.savefig("./analysis_plots/runtime_박스플롯.png")
+plt.clf()
 
 sns.boxplot(x=train['revenue'])
-plt.savefig("revenue_박스플롯.png")
+plt.savefig("./analysis_plots/revenue_박스플롯.png")
 plt.clf()
 
 sns.boxplot(x=train['omdb_rate'])
-plt.savefig("omdb_rate_박스플롯.png")
+plt.savefig("./analysis_plots/omdb_rate_박스플롯.png")
 plt.clf()
 
 # 모델 Building
 
 # 독립 변수와 종속 변수 분리
-X = train.drop(columns=['omdb_rate'])  # 'revenue' 열을 종속 변수로 사용
+X = train.drop(columns=['omdb_rate']) # 'omdb_rate' 열을 종속 변수로 사용
 y = train['omdb_rate']
 
 # splitting the data into training and validation to check validity of the model
@@ -107,8 +96,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_
 print(f"학습 데이터 크기: {X_train.shape}")
 print(f"테스트 데이터 크기: {X_test.shape}")
 
-print(X_train)
-
 # Linear Model
 def rmsle(y,y0): return np.sqrt(np.mean(np.square(np.log1p(y)-np.log1p(y0)))) 
 reg = LinearRegression()
@@ -117,33 +104,5 @@ y_pred = lin_model.predict(X_test)
 print('RMSLE score for linear model is {}'.format(rmsle(y_test, y_pred)))
 pred1 = lin_model.predict(X_test)
 
-print(f'예측된 평점: {pred1[0]}')
-
 with open('lin_model.pkl', 'wb') as f:
     pickle.dump(lin_model, f)
-
-knn = KNeighborsRegressor(n_neighbors = 5)
-knn_model = knn.fit(X_train, y_train)
-knn_y_pred = knn.predict(X_test)
-print('RMSLE score for k-NN model is {}'.format(rmsle(y_test, knn_y_pred)))
-pred2 = knn.predict(X_test)
-
-print(f'예측된 평점: {pred2[0]}')
-
-rf = RandomForestRegressor()
-rf_model = rf.fit(X_train, y_train)
-rf_y_pred = rf.predict(X_test)
-print('RMSLE score for Random Forest model is {}'.format(rmsle(y_test, rf_y_pred)))
-pred3 = rf.predict(X_test)
-
-print(f'예측된 평점: {pred3[0]}')
-
-dic = pd.DataFrame({
-    # "budget": [74000000],
-    "runtime": [91],
-    "revenue": [1159457503],
-})
-
-print(lin_model.predict(dic))
-print(knn_model.predict(dic))
-print(rf_model.predict(dic))
